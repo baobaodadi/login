43a8fbbcd86bc5be1a331b7e90f8eaa9aa8ece48
dadi <675834325@qq.com>
2018-05-15 17:52:39 +0800

    add mysql

--- myapp.py
+++ myapp.py
@@ -1,8 +1,25 @@
 
 from flask import Flask, session, redirect, url_for, escape, request, render_template, make_response, jsonify
 from flask_cors import *
+import MySQLdb
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:new123@localhost/mysite'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+    email = db.Column(db.String(320), unique=True)
+    phone = db.Column(db.String(32), nullable=False)
+
+    def __init__(self, username, email, phone):
+        self.username = username
+        self.email = email
+        self.phone = phone
 
 
 CORS(app, supports_credentials=True)
@@ -13,11 +30,12 @@ def login():
         username = request.form['username']
         password = request.form['password']
 
-    if username == 'admin' and password == 'new123':
- # return resp
+    select_=User.query.filter_by(username=username).first()
+
+    if password == select_.email:
+            print(password)
             return jsonify({"code":0,"message":"OK","data":{"msg":1}})
     else:
-            # return redirect(url_for('error'))
             return jsonify({"code":1, "message": "error", "data": {"msg": 0}})
 
 if __name__ == '__main__':
